105f20626f70b5df96bee5af7d315a80c93b2e76
Steve Antonio Silva <steveantoniosilva@gmail.com>
2022-06-20 15:43:02 -0400

    added kobe + update tooltips

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import "./styles.css";
-import React from "react";
-import { useState } from "react";
-import Button from "./Button";
-import Tooltip from "./Tooltip";
+import './styles.css';
+import React from 'react';
+import { useState } from 'react';
+import Button from './Button';
+import Tooltip from './Tooltip';
 
 function App() {
-  const [player, setPlayer] = useState("landing");
+  const [player, setPlayer] = useState('landing');
   return (
     <div className={`react-root ${player}`}>
       <div>
-        <Tooltip direction='bottom1' content="' The Goat '">
+        <Tooltip direction='bottom1' content=' The Goat '>
           <Button player='mj' setPlayer={setPlayer} />
         </Tooltip>
-        <Tooltip direction='bottom2' content="' Magic '">
-          <Button player='magic' setPlayer={setPlayer} />
+        <Tooltip direction='bottom2' content=' Kobe '>
+          <Button player='kobe' setPlayer={setPlayer} />
         </Tooltip>
-        <Tooltip direction='bottom3' content="' Legend '">
+        <Tooltip direction='bottom3' content=' Legend '>
           <Button player='bird' setPlayer={setPlayer} />
         </Tooltip>
-        <Tooltip direction='bottom4' content="' Diesel '">
+        <Tooltip direction='bottom4' content=' Diesel '>
           <Button player='shaq' setPlayer={setPlayer} />
         </Tooltip>
-        <Tooltip direction='bottom5' content="' The Answer '">
+        <Tooltip direction='bottom5' content=' The Answer '>
           <Button player='iverson' setPlayer={setPlayer} />
         </Tooltip>
       </div>
